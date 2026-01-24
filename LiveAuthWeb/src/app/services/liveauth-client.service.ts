24674e35235688dd8ca859c2bd944fb42133a330
scott <dulzuradev@gmail.com>
2026-01-24 12:19:15 -0800

    keys update

--- LiveAuthWeb/src/app/services/liveauth-client.service.ts
+++ LiveAuthWeb/src/app/services/liveauth-client.service.ts
@@ -82,7 +82,7 @@ export class LiveAuthClientService {
   private readonly baseUrl = BASE_API_URL;
 
   private readonly headers = new HttpHeaders({
-    'X-LW-Public': 'la_pk_UU2AxW3NYQPjb3Z6C8cxdKDA',
+    'X-LW-Public': 'la_pk_MlbE58oakBqwbBgnswFOz3yD',
     'Content-Type': 'application/json'
   });
 
