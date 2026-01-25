6ea7c1bfa08bba0b91723d7def6305c042aaa3fc
scott <dulzuradev@gmail.com>
2026-01-24 16:27:07 -0800

    rowversion update

--- LiveAuthCore/Data/Entities/BillingSubscriptions.cs
+++ LiveAuthCore/Data/Entities/BillingSubscriptions.cs
@@ -20,6 +20,6 @@ public class BillingSubscription
     public DateTime ExpiresAt { get; set; }
     public DateTime CreatedAt { get; set; }
     
-    public byte[]? RowVersion { get; set; } = Array.Empty<byte>();
+    public byte[]? RowVersion { get; set; } = Guid.NewGuid().ToByteArray();
 
 }
