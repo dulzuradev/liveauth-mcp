c5930242feee244e1c6bf6ef728173c6d11ff1f4
scott <dulzuradev@gmail.com>
2026-01-25 14:06:05 -0800

    pro sub amt change

--- LiveAuthCore/Controllers/SubscriptionController.cs
+++ LiveAuthCore/Controllers/SubscriptionController.cs
@@ -66,7 +66,7 @@ namespace LiveAuthCore.Controllers
             // v1 pricing (simple + explicit)
             var amountSats = request.Plan switch
             {
-                "pro" => 100_00, // ~ $50–60 at current rates
+                "pro" => 1_000, // ~ $50–60 at current rates
                 _ => throw new ArgumentException("Unknown plan")
             };
 
