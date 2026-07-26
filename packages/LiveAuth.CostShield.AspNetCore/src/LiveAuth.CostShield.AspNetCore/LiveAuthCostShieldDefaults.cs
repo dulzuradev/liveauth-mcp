f83aa701dd82252cb45514e821233579cba2b248
scott <dulzuradev@gmail.com>
2026-07-26 07:20:52 -0700

    chore(costshield): move ASP.NET package to public repo

--- packages/LiveAuth.CostShield.AspNetCore/src/LiveAuth.CostShield.AspNetCore/LiveAuthCostShieldDefaults.cs
+++
@@ -1,9 +0,0 @@
-namespace LiveAuth.CostShield.AspNetCore;
-
-internal static class LiveAuthCostShieldDefaults
-{
-    public const string HttpClientName =
-        "LiveAuth.CostShield.AspNetCore";
-    public const int MaximumTokenLength = 8 * 1024;
-    public const string TokenType = "costshield+jwt";
-}
